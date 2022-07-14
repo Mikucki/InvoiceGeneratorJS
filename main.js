806eb0247b3d13be0d4fa12d84afb5b005755dda
Mikucki <piotr.mikucki@op.pl>
2022-07-14 16:09:21 -0400

    tutorial text and invoice

---
+++ main.js
@@ -0,0 +1,15 @@
+const weapons = ['candlestick', 'lead pipe', 'revolver']
+
+const makeBroke = function(item) {
+    return `broken ${weapons}`
+}
+
+
+const brokenWeapons = _.map(weapons, makeBroke)
+
+brokenWeapons;
+
+
+
+const abc1 = [];
+
